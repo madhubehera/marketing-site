d890959dfa176330fcd71892374d3365a58faba8
madhubehera <129487428+madhubehera@users.noreply.github.com>
2025-04-13 15:21:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Just a demo script for now
+console.log("Welcome to MarketPro!");
+document.getElementById('queryForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you! Your query has been submitted.');
+  });
+  
